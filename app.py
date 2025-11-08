7b19e499029dd18c9f0901259689b9296e1c84da
christopherbrunswick <Cbruns34@gmail.com>
2025-11-08 12:03:51 -0500

    Commented out whitenoise

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from whitenoise import WhiteNoise
+#from whitenoise import WhiteNoise
 import datetime
 
 
 app = Flask(__name__, static_folder='static')
 
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/")
+#app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/")
 
 @app.route('/')
 def index():
